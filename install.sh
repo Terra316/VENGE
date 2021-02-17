40fa154bc3d7370070a07c514856ba556bb8a6a4
Terra316 <74781119+Terra316@users.noreply.github.com>
2021-02-17 09:25:56 -0600

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+
+
+expect "The layout of keyboards varies per country, with some countries having multiple 
+common layouts. Please select the country of origin for the keyboard of this
+computer."
+send "31\r"
