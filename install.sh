ec958157e1b9d5895126fb8d8afbd53410dfba61
Terra316 <74781119+Terra316@users.noreply.github.com>
2021-02-17 09:46:21 -0600

    Delete install.sh

--- install.sh
+++
@@ -1,6 +0,0 @@
-sudo apt install -y nvidia-driver-455
-
-expect "The layout of keyboards varies per country, with some countries having multiple 
-common layouts. Please select the country of origin for the keyboard of this
-computer."
-send "31\r"
